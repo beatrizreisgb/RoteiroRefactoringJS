206f507b82c2fe9bc8f661b3c3215bf8497d4b7d
beatrizreisgb <beatriz.barbosa@dcc.ufmg.br>
2024-12-12 20:39:00 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -29,17 +29,21 @@ function gerarFaturaStr (fatura, pecas) {
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
   
+    function getPeca(apresentacao) {
+      return pecas[apresentacao.id];
+    }
+
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = calcularTotalApresentacao(apre, peca);
+      // const peca = getPeca(apre);
+      let total = calcularTotalApresentacao(apre, getPeca(apre));
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
