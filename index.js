0a98a320d4dcb8d5bd3a9429e5876638fa8765e3
beatrizreisgb <beatriz.barbosa@dcc.ufmg.br>
2024-12-12 20:44:39 -0300

    Commit 3 - Mais Extract Functions

--- index.js
+++ index.js
@@ -22,31 +22,39 @@ function gerarFaturaStr (fatura, pecas) {
       }
       return total;
     }
-    let totalFatura = 0;
-    let creditos = 0;
-    let faturaStr = `Fatura ${fatura.cliente}\n`;
-    const formato = new Intl.NumberFormat("pt-BR",
-                          { style: "currency", currency: "BRL",
-                            minimumFractionDigits: 2 }).format;
-  
+
     function getPeca(apresentacao) {
       return pecas[apresentacao.id];
     }
 
-    for (let apre of fatura.apresentacoes) {
-      // const peca = getPeca(apre);
-      let total = calcularTotalApresentacao(apre, getPeca(apre));
-  
-      // créditos para próximas contratações
+    function calcularCredito(apre) {
       creditos += Math.max(apre.audiencia - 30, 0);
       if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
+      return creditos;   
+    }
+
+    function formatarMoeda(valor) {
+      return new Intl.NumberFormat("pt-BR",
+        { style: "currency", currency: "BRL",
+          minimumFractionDigits: 2 }).format(valor/100);
+    }
+
+    let totalFatura = 0;''
+    let creditos = 0;
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
+
+    for (let apre of fatura.apresentacoes) {
+      // const peca = getPeca(apre);
+      let total = calcularTotalApresentacao(apre, getPeca(apre));
+
+      creditos =  calcularCredito(apre);
   
       // mais uma linha da fatura
-      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
-    faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
+    faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
     return faturaStr;
   }
